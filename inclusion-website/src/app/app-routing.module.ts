55513d29a43c16e82207db360152b21045fcb5f2
Thibaut Mouton <thibaut.mouton@isen.yncrea.fr>
2019-04-01 11:23:57 +0200

    Initialisation

--- inclusion-website/src/app/app-routing.module.ts
+++ inclusion-website/src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AdminComponent } from "./admin/admin.component";
 import { AppComponent } from "./app.component";
-import { EnteteComponent } from "./entete/entete.component";
+import { AccueilComponent } from "./accueil/accueil.component";
 import { ContentAdminComponent } from "./content-admin/content-admin.component";
 
 const routes: Routes = [
-  { path: "", component: EnteteComponent },
+  { path: "", component: AccueilComponent },
   { path: "admin", component: AdminComponent },
   { path: "admin/:id", component: ContentAdminComponent }
 ];
