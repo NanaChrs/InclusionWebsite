3b1d96eb5c68f3a1e042b3b08eb2c8ad8d933ecf
Thibaut Mouton <thibaut.mouton@isen.yncrea.fr>
2019-04-01 14:21:13 +0200

    Header fonctionnel + favicon

--- inclusion-website/src/app/app-routing.module.ts
+++ inclusion-website/src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ import { AccueilComponent } from "./accueil/accueil.component";
 import { ContentAdminComponent } from "./content-admin/content-admin.component";
 
 const routes: Routes = [
-  { path: "", component: AccueilComponent },
+  { path: "**", redirectTo: "/accueil" },
+  { path: "accueil" , component: AccueilComponent},
   { path: "admin", component: AdminComponent },
   { path: "admin/:id", component: ContentAdminComponent }
 ];
