4b2bbbc79e54b5a3de42f7e91ccb83e4f1310a4e
Clément Béghin <c.beghin02@gmail.com>
2019-04-01 15:38:12 +0200

    Bandeau + header + archi dossier

--- inclusion-website/src/app/app.module.ts
+++ inclusion-website/src/app/app.module.ts
@@ -1,20 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EnteteComponent } from './entete/entete.component';
+import { AdminComponent } from './admin/admin.component';
+import { NavAdminComponent } from './nav-admin/nav-admin.component';
+import { LoginComponent } from './login/login.component';
+import { TextContentComponent } from './text-content/text-content.component';
+import { PhotoContentComponent } from './photo-content/photo-content.component';
+import { ContentAdminComponent } from './content-admin/content-admin.component';
+import { MyDialogComponent } from './my-dialog/my-dialog.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 
+import { MatButtonModule } from '@angular/material';
+import { MatCardModule } from '@angular/material';
+import { MatDialogModule } from '@angular/material';
+import { EnteteComponent } from './entete/entete.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AccueilComponent } from './accueil/accueil.component';
 @NgModule({
   declarations: [
     AppComponent,
+    AdminComponent,
+    NavAdminComponent,
+    LoginComponent,
+    TextContentComponent,
+    PhotoContentComponent,
+    ContentAdminComponent,
+    MyDialogComponent,
+    SlideshowComponent,
     EnteteComponent,
-    SlideshowComponent
+    AccueilComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MatCardModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+  ],
+  entryComponents: [
+    MyDialogComponent
+  ],
+
+  exports: [
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- inclusion-website/src/app/entete/entete.component.ts
+++ inclusion-website/src/app/entete/entete.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { MyDialogComponent } from '../my-dialog/my-dialog.component';
 
 @Component({
   selector: 'app-entete',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnteteComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(public dialog: MatDialog) {}
+ 
+  dialogResult = "";
   ngOnInit() {
   }
 
+  openDialog() {
+    let dialogRef = this.dialog.open(MyDialogComponent, {
+      width: '25%',
+      height: '25%',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {console.log('Dialog closed: ${result');
+    this.dialogResult = result;
+  })
+  }
 }
