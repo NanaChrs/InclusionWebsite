f683a642ff5ec98cd159fa83e933d004b12e208b
Maxfont <maxime.fontaine@isen.yncrea.fr>
2019-04-02 17:32:52 +0200

    animation diapo

--- inclusion-website/src/app/accueil/accueil.component.ts
+++ inclusion-website/src/app/accueil/accueil.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./accueil.component.css']
 })
 export class AccueilComponent implements OnInit {
+  public animation = false;
   //duré d'une photo dans le diapo
-  duration = 10;
+  duration = 4;
   i = 0;
   //hauteur du diaporama
   height = 650;
@@ -18,27 +19,27 @@ export class AccueilComponent implements OnInit {
   @ViewChild('myslideshow') slideshow;
   
 
-  execute = function(){
-
-    //on applique la hauteur au diapo et on le lance
-    if ((this.height)>0){
-      this.setContainerStyle();
-      this.play();
-    }
-  }
- 
-
   // fonction pour appliquer la hauteur du dipoa et la position relative
   setContainerStyle = function(){
-    this.slideshowcontainer.nativeElement.style.position = 'relative';
     this.slideshowcontainer.nativeElement.style.height = this.height + 'px';
+    this.slideshow.nativeElement.style.background =  'url('+this.slideshow.nativeElement.children[this.i].src+') center';
+    this.slideshow.nativeElement.style.backgroundSize = 'cover';
   }
 
 
   play = function(){
+
+
+    // on incremente i afin de changer la photo qui sera affichée dans le slideshow
+    this.i++;
+    //quand on arrive à la derniere diapo, on retourne à la premiere
+    if(this.i>this.slideshow.nativeElement.childElementCount-1)
+    {
+      this.i=0;
+    } 
+
     // application de la classe fadeOut pour application la transition apparition petit à petit
     this.slideshow.nativeElement.className = 'fadeOut';
-
 
     setTimeout(() => {
       // changement du background de l'element slideshow
@@ -49,14 +50,6 @@ export class AccueilComponent implements OnInit {
     }, 2000);
 
 
-    // on incremente i afin de changer la photo qui sera affichée dans le slideshow
-    this.i++;
-    //quand on arrive à la derniere diapo, on retourne à la premiere
-    if(this.i==this.slideshow.nativeElement.childElementCount)
-    {
-      this.i=0;
-    }
-
 
     // fonction qui permet de lancer le diapo en boucle 
     setTimeout(() => {
@@ -65,11 +58,32 @@ export class AccueilComponent implements OnInit {
   }
 
 
+  onNext(){
+    this.i++;
+    if(this.i>this.slideshow.nativeElement.childElementCount-1){ this.i=0; }
+    this.slideshow.nativeElement.style.background =  'url('+this.slideshow.nativeElement.children[this.i].src+') center';
+    this.slideshow.nativeElement.style.backgroundSize = 'cover';
+  }
+
+  onPrev(){
+    this.i--;
+    if(this.i<0){ this.i=this.slideshow.nativeElement.childElementCount-1; }
+    this.slideshow.nativeElement.style.background =  'url('+this.slideshow.nativeElement.children[this.i].src+') center';
+    this.slideshow.nativeElement.style.backgroundSize = 'cover';
+  }
+
+
   constructor() { }
 
 
   ngOnInit() {
-    this.execute();
+    if ((this.height)>0){
+      this.setContainerStyle();
+    }
+    if(this.animation){
+      this.play();
+    }
+    
   }
 
 }
