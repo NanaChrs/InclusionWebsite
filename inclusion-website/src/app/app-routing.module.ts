6e6a3e917c2f4e921e73c18fb4542d39122789b7
Thibaut Mouton <thibaut.mouton@isen.yncrea.fr>
2019-04-09 17:14:54 +0200

    Onglet equipe

--- inclusion-website/src/app/app-routing.module.ts
+++ inclusion-website/src/app/app-routing.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from "./app.component";
 import { AccueilComponent } from "./vue-client/accueil/accueil.component";
 import { MenuComponent } from "./vue-client/menu/menu.component";
 import { ContentAdminComponent } from "./admin/content-admin/content-admin.component";
+import { EquipeComponent } from './vue-client/equipe/equipe.component';
 
 const routes: Routes = [
   { path: "accueil", component: AccueilComponent, pathMatch: 'full' },
   { path: "menus", component: MenuComponent, pathMatch: 'full' },
+  { path: "equipe", component: EquipeComponent, pathMatch: 'full' },
   { path: "admin/:url", component: ContentAdminComponent, pathMatch: 'full' },
   { path: "admin", component: AdminComponent, pathMatch: 'full' },
   { path: "**", redirectTo: "/accueil" }
