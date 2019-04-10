9b2afa45125ece1c517a13cd18ad1c1336aea952
Jules <jules.guiot@isen.yncrea.fr>
2019-04-10 17:13:19 +0200

    try commit

--- inclusion-website/src/app/service/authentification.service.ts
+++ inclusion-website/src/app/service/authentification.service.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { HttpClientService, Login } from '../service/httpclientservice.service';
+import { User } from '../_models';
+import { LoginComponent } from '../login/login.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthentificationService {
+  constructor(private cookieService: CookieService,private http: HttpClientService) { }
 
-  constructor() { }
 
   isUserLoggedIn() {
+    
     let user = sessionStorage.getItem('username')
-    console.log(!(user === null))
-    return !(user === null)
+    return (user);
   }
+  
 
   logOut() {
     sessionStorage.removeItem('username')
+    this.cookieService.delete('token');
   }
 }
