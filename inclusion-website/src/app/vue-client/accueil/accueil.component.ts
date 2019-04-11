b9c8b074fe262aa529e74a4cd18419e1c4ad4d7f
NanaChrs <mathilde.christiaens@isen.yncrea.fr>
2019-04-11 17:59:24 +0200

    affichage qui ne fonctionne pas

--- inclusion-website/src/app/vue-client/accueil/accueil.component.ts
+++ inclusion-website/src/app/vue-client/accueil/accueil.component.ts
@@ -40,14 +40,14 @@ export class AccueilComponent implements OnInit {
       this.textContent = this.pageContent["text-content"];
       this.imageContent = this.pageContent["photo-content"];
     });
-    // application de la hauteur du dipoa et la position relative
-    if (this.height > 0) {
-      this.slideshowcontainer.nativeElement.style.height = this.height + "px";
-      this.setContainerStyle();
-    }
   }
 
   ngAfterViewInit() {
+    // application de la hauteur du dipoa et la position relative
+    if (this.height > 0) {
+      // this.slideshowcontainer.nativeElement.style.height = this.height + "px";
+      this.setContainerStyle();
+    }
     // lancement du diapo
     this.play();
     // lancement de la fonction permettant de verifier les modifications "accessibilité"
@@ -64,14 +64,16 @@ export class AccueilComponent implements OnInit {
 
   // fonction pour appliquer la hauteur du dipoa et la position relative
   setContainerStyle = function() {
-    if(this.slideshow.nativeElement.children[this.i]==undefined && this.animationOFF){
+    console.log("");
+    if (this.slideshow.nativeElement == undefined) {
       setTimeout(() => {
         this.setContainerStyle();
       }, 100);
     }
-    // this.slideshowcontainer.nativeElement.style.height = this.height + "px";
-    this.slideshow.nativeElement.style.background =  'url('+this.slideshow.nativeElement.children[this.i].src+') center';
-    this.slideshow.nativeElement.style.backgroundSize = 'cover';
+    this.slideshowcontainer.nativeElement.style.height = this.height + "px";
+    this.slideshow.nativeElement.style.background =
+      "url(" + this.slideshow.nativeElement.children[this.i].src + ") center";
+    this.slideshow.nativeElement.style.backgroundSize = "cover";
   };
 
   play = function() {
