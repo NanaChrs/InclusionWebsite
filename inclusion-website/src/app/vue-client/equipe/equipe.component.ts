2e83310f450af2e3b136d4f0eb5e4614b1a72ae2
NanaChrs <mathilde.christiaens@isen.yncrea.fr>
2019-04-12 10:37:11 +0200

    equipe full from json

--- inclusion-website/src/app/vue-client/equipe/equipe.component.ts
+++ inclusion-website/src/app/vue-client/equipe/equipe.component.ts
@@ -7,53 +7,29 @@ import { JsoncontentService } from "../../admin/content-admin/jsoncontent.servic
   styleUrls: ["./equipe.component.css"]
 })
 export class EquipeComponent implements OnInit {
-  //   alt : "photo d'identité de ";
-  //   public content = [
-  //     {
-  //     nom: "Jules",
-  //     fonction :"Gitkraken destructeur",
-  //   },
-  // {
-  //   nom:"Clément",
-  //   fonction:"Json pro user"
-  // },
-  // {
-  //   nom:"Mathilde",
-  //   fonction:"La meuf fatiguée"
-  // },
-  // {
-  //   nom:"Thibaut",
-  //   fonction:"Le mec du css"
-  // },
-  // {
-  //   nom:"Maxime",
-  //   fonction:"Mec cool"
-  // },
-  // {
-  //   nom:"Lisa",
-  //   fonction:"Meuf cool"
-  // }]
-
   pageContent: String[];
   textContent: String[];
   imageContent: String[];
   equipe: String[][];
   descriptionGlobale: String;
   descriptions: String[];
+  nousRejoindre: String[];
   nom: String;
 
-  constructor(private jsonContentService: JsoncontentService) { }
+  constructor(private jsonContentService: JsoncontentService) {}
 
   ngOnInit() {
-    this.jsonContentService.getPageByName('equipe').subscribe((page) => {
+    this.jsonContentService.getPageByName("equipe").subscribe(page => {
       this.pageContent = page;
       this.textContent = this.pageContent["text-content"];
-      this.descriptionGlobale = this.textContent[0][0]['contenu'];
+      this.descriptionGlobale = this.textContent[0][0]["contenu"];
       this.imageContent = this.pageContent["photo-content"];
       this.descriptions = this.textContent;
       this.descriptions.splice(0, 1);
-      console.log(this.descriptions);
+      this.nousRejoindre = this.descriptions.splice(
+        this.descriptions.length - 1,
+        1
+      );
     });
   }
 }
-
