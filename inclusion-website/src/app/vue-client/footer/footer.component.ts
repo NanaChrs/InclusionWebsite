a3aca44b03f1868ff0d0ad693fef1ed53653eea8
NanaChrs <mathilde.christiaens@isen.yncrea.fr>
2019-04-17 16:46:57 +0200

    équipe + footer avec param généraux

--- inclusion-website/src/app/vue-client/footer/footer.component.ts
+++ inclusion-website/src/app/vue-client/footer/footer.component.ts
@@ -1,32 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
-import { JsoncontentService } from '../../admin/content-admin/jsoncontent.service';
-
+import { Component, OnInit } from "@angular/core";
+import { CookieService } from "ngx-cookie-service";
+import { JsoncontentService } from "../../admin/content-admin/jsoncontent.service";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.css"]
 })
 export class FooterComponent implements OnInit {
   pageContent: String[];
   textContent: String[];
-  semaine = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+  semaine = [
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi",
+    "Dimanche"
+  ];
   horaire: String;
+  private param: any;
+  private mail: String;
+  private tel: String;
+  private adresse: String[] = [];
+  private insta: String;
+  private fb: String[];
+  private uber: String;
+  private tripAd: String;
 
-
-
-  constructor(private jsonContentService: JsoncontentService) { }
+  constructor(private jsonContentService: JsoncontentService) {}
 
   ngOnInit() {
     this.jsonContentService.getPageByName("footer").subscribe(page => {
       this.pageContent = page;
       this.textContent = this.pageContent["text-content"];
-      this.horaire = this.textContent[1].slice(1, 8);
+      this.horaire = this.textContent[0].slice(1, 8);
+    });
+    this.jsonContentService.getPageByName("param").subscribe(page => {
+      this.param = page;
+      this.mail = page["text-content"][0][0]["contenu"];
+      this.tel = page["text-content"][0][1]["contenu"];
+      this.adresse.push(page["text-content"][1][0]["contenu"]);
+      this.adresse.push(page["text-content"][1][1]["contenu"]);
+      this.adresse.push(page["text-content"][1][2]["contenu"]);
+      this.insta = page["text-content"][2][0]["contenu"];
+      this.fb = page["text-content"][2][1]["contenu"];
+      this.uber = page["text-content"][2][2]["contenu"];
+      this.tripAd = page["text-content"][2][3]["contenu"];
     });
   }
 
-  ngAfterViewInit() {
-
-  }
+  ngAfterViewInit() {}
 }
