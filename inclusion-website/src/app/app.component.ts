a5e224c8deb4b6b71c7fdcfecd5bd4973c771c5d
NanaChrs <mathilde.christiaens@isen.yncrea.fr>
2019-04-17 15:06:11 +0200

    essai paramètres généraux + json

--- inclusion-website/src/app/app.component.ts
+++ inclusion-website/src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { JsoncontentService } from "./admin/content-admin/jsoncontent.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'inclusion';
-}
+export class AppComponent implements OnInit {
+  title = "inclusion";
+  public paramGeneraux: String[];
 
+  constructor(private jsonContentService: JsoncontentService) {}
+
+  ngOnInit(): void {
+    this.jsonContentService.getPageByName("param").subscribe(page => {
+      this.paramGeneraux = page["param"];
+    });
+  }
+}
