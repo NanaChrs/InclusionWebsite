9f5b0ac01b3593185a1cb52fede079f932006f30
Clément Béghin <c.beghin02@gmail.com>
2019-04-18 21:10:47 +0200

    online

--- inclusion-website/src/app/service/httpclientservice.service.ts
+++ inclusion-website/src/app/service/httpclientservice.service.ts
@@ -4,22 +4,22 @@ import { User } from "../_models";
 import { Mail } from "../_models";
 
 export class Login {
-  constructor() {}
+  constructor() { }
 }
 
 @Injectable({
   providedIn: "root"
 })
 export class HttpClientService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) { }
 
   postLogin(user: User) {
     // console.log(Login);
-    // return this.httpClient.post<Login[]>('https://dev.inclusion-restaurant.fr/api/login', user);
-    return this.httpClient.post<Login[]>(
-      "http://localhost:8000/api/login",
-      user
-    );
+    return this.httpClient.post<Login[]>('https://dev.inclusion-restaurant.fr/api/login', user);
+    // return this.httpClient.post<Login[]>(
+    //   "http://localhost:8000/api/login",
+    //   user
+    // );
   }
   postMail(mail: Mail) {
     // console.log(Login);
