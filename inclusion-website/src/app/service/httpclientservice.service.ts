45656c7c61a3af9da592c1ce52647bd4abaf2f63
NanaChrs <mathilde.christiaens@isen.yncrea.fr>
2019-04-18 14:51:40 +0200

    problème des bières réglé

--- inclusion-website/src/app/service/httpclientservice.service.ts
+++ inclusion-website/src/app/service/httpclientservice.service.ts
@@ -1,32 +1,32 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { User } from '../_models';
-import { Mail } from '../_models';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { User } from "../_models";
+import { Mail } from "../_models";
 
 export class Login {
-  constructor(
-  ) { }
+  constructor() {}
 }
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class HttpClientService {
-
-  constructor(
-    private httpClient: HttpClient
-  ) {
-  }
+  constructor(private httpClient: HttpClient) {}
 
   postLogin(user: User) {
     // console.log(Login);
-    return this.httpClient.post<Login[]>('https://dev.inclusion-restaurant.fr/api/login', user);
-    // return this.httpClient.post<Login[]>('http://localhost:8000/api/login', user);
+    // return this.httpClient.post<Login[]>('https://dev.inclusion-restaurant.fr/api/login', user);
+    return this.httpClient.post<Login[]>(
+      "http://localhost:8000/api/login",
+      user
+    );
   }
   postMail(mail: Mail) {
     // console.log(Login);
-    return this.httpClient.post<Login[]>('https://dev.inclusion-restaurant.fr/api/contact', mail);
+    return this.httpClient.post<Login[]>(
+      "https://dev.inclusion-restaurant.fr/api/contact",
+      mail
+    );
     //console.log(Contact)
     // return this.httpClient.post<Login[]>('http://localhost:8000/api/contact', mail);
   }
